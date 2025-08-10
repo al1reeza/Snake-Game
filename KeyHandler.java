dc7ece5c6d987739fbbad48d2da68b8b508312bd
al1reeza <a_al1reza@hotmail.com>
2025-08-10 11:39:09 -0400

    Update KeyHandler.java

--- KeyHandler.java
+++ KeyHandler.java
@@ -9,6 +9,7 @@ public class KeyHandler implements KeyListener{
 	
 	// We don't need this method for our game
 	public void keyTyped(KeyEvent e) {
+		// no-op
 	}
 
 	// Handles the case when a keyboard key is pressed
@@ -22,49 +23,38 @@ public class KeyHandler implements KeyListener{
 		 */
 		if(code == KeyEvent.VK_W) {
 			up = true;
-		
 		}
 		if(code == KeyEvent.VK_S) {
 			down = true;
-			
 		}
 		if(code == KeyEvent.VK_A) {
 			left = true;
-			
 		}
 		if(code == KeyEvent.VK_D) {
 			right = true;
-			
 		}
-		
 	}
 
 	// Handles the case when a keyboard key is released
 	public void keyReleased(KeyEvent e) {
 		// Stores the integer form of the key we just pressed in the code variable
 		int code = e.getKeyCode();
-		
 		/*
 		 * The series of if statement determines the direction of our entity, in this
 		 * specific game and case the snake based on the key released
 		 */
 		if(code == KeyEvent.VK_W) {
 			up = false;
-			
 		}
 		if(code == KeyEvent.VK_S) {
 			down = false;
-			
 		}
 		if(code == KeyEvent.VK_A) {
 			left = false;
-			
 		}
 		if(code == KeyEvent.VK_D) {
 			right = false;
-			
 		}
-		
 	}
-
 }
+
